e55d3417bc8ce7458d051153a8a6fe974679b7cf
iiishita05 <ishitasrivastava003@gmail.com>
2025-04-02 20:05:59 +0530

    chnages

--- Question1/social/src/api.js
+++ Question1/social/src/api.js
@@ -1,16 +1,16 @@
-const API_URL = ""; 
+const API_URL = "http://20.244.56.144/evaluation-service";
 
 export const fetchUsers = async () => {
   const response = await fetch(`${API_URL}/users`);
   return response.json();
 };
 
-export const fetchPosts = async () => {
-  const response = await fetch(`${API_URL}/posts`);
+export const fetchPosts = async (userId) => {
+  const response = await fetch(`${API_URL}/users/${userId}/posts`);
   return response.json();
 };
 
-export const fetchComments = async () => {
-  const response = await fetch(`${API_URL}/comments`);
+export const fetchComments = async (postId) => {
+  const response = await fetch(`${API_URL}/posts/${postId}/comments`);
   return response.json();
 };
